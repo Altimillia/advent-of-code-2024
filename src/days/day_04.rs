8f13fbd9c656488636db1e29462af343e3ad579c
deweesem0225 <deweesem0225@gmail.com>
2024-12-04 13:40:02 -0600

    Day 4 done

--- src/days/day_04.rs
+++ src/days/day_04.rs
@@ -1,7 +1,10 @@
 ﻿use std::collections::HashMap;
 use std::fmt::Display;
 use crate::domain::point::Point;
-
+use crate::domain::point::NORTHEAST;
+use crate::domain::point::SOUTHEAST;
+use crate::domain::point::NORTHWEST;
+use crate::domain::point::SOUTHWEST;
 pub fn part_one(input: String) -> impl Display {
     let grid = WordGrid::parse(input);
 
@@ -10,7 +13,10 @@ pub fn part_one(input: String) -> impl Display {
 }
 
 pub fn part_two(input: String) -> impl Display {
-    0
+    let grid = WordGrid::parse(input);
+
+    let result = grid.search_cross_word();
+    result
 }
 
 struct WordGrid {
@@ -35,6 +41,46 @@ impl WordGrid {
         }
 
         WordGrid { grid: map, size: total_size }
+    }
+
+    fn search_cross_word(&self) -> i32 {
+        // search for A
+        // If we find one, then look for in the four diagonal.
+        // If its successful then add the points to a list to avoid finding the same one again
+        let mut count = 0;
+
+        for y in (0..self.size.y).rev() {
+            for x in 0..self.size.x {
+                let current_point = Point::new(x, y);
+                let current_letter = *self.grid.get(&current_point).unwrap();
+                if 'A' == current_letter {
+                    if(self.check_diagonal(current_point + NORTHEAST, current_point + SOUTHWEST) && self.check_diagonal(current_point + NORTHWEST, current_point + SOUTHEAST)){
+                        count += 1;
+                    }
+                }
+            }
+        }
+
+        count
+    }
+
+    fn check_diagonal(&self, start: Point, end:Point) -> bool {
+        if(!self.grid.contains_key(&start) || !self.grid.contains_key(&end)){
+            return false;
+        }
+        let start_letter = *self.grid.get(&start).unwrap();
+        let end_letter = *self.grid.get(&end).unwrap();
+
+        if(start_letter == 'M' && end_letter == 'S')
+        {
+            return true;
+        }
+        if(start_letter == 'S' && end_letter == 'M')
+        {
+            return true;
+        }
+
+        false
     }
 
     fn search_word(&self, word: &str) -> i32 {
@@ -100,5 +146,23 @@ MXMXAXMASX"#;
         let word_grid = WordGrid::parse(input.to_string());
         let result = word_grid.search_word("XMAS");
         assert_eq!(result, 18);
+    }
+
+    #[test]
+    fn finding_x_mas_in_word_grid() {
+        let input = r#"MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX"#;
+
+        let word_grid = WordGrid::parse(input.to_string());
+        let result = word_grid.search_cross_word();
+        assert_eq!(result, 9);
     }
 }
